4cbdec346198fdbb859183ee07a049129f89d6f6
Vikram Jadhav <vikramcse.10@gmail.com>
2016-04-18 20:47:26 +0530

    added connect to peer functionality with toast messages

--- app/components/Panel.js
+++ app/components/Panel.js
@@ -5,6 +5,10 @@ class Panel extends React.Component {
   constructor(props) {
     super(props);
 
+    this.renderConnected = this.renderConnected.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.connect = this.connect.bind(this);
+
     this.state = {
       peer: new Peer({key: this.props.opts.peerjs_key}),
       my_id: '',
@@ -26,7 +30,8 @@ class Panel extends React.Component {
 
     // The other peer is trying to connect
     this.state.peer.on('connection', function(connection) {
-      toastr.info('someone connected');
+      toastr.success('someone connected');
+
       // After other peer got connected then change the state
       // and store the connection information. As the setState
       // is Async we need to pass a callback to setState as
@@ -46,18 +51,66 @@ class Panel extends React.Component {
     }.bind(this));
   }
 
+  renderConnected() {
+    return (
+      <div>
+        <input type="file" name="file" className="mui--hide" onChange={this.sendFile} />
+        <label htmlFor="file" className="mui-btn mui-btn--small mui-btn--primary mui-btn--fab">+</label>
+      </div>
+    );
+  }
+
+  handleTextChange(event) {
+    this.setState({
+      peer_id: event.target.value
+    });
+  }
+
+  connect() {
+    // here get the peer_id from text box and pass it to
+    // the method of connect of peer object
+    var peer_id = this.state.peer_id;
+    var connection = this.state.peer.connect(peer_id);
+
+    this.setState({
+      conn: connection
+    }, function() {
+      this.state.conn.on('open', function() {
+        toastr.success('You are connected to your Opponent');
+
+        this.setState({
+          connected: true
+        });
+      }.bind(this));
+    });
+  }
+
+  renderNotConnected() {
+    return (
+      <div>
+        <div className="mui-textfield" onChange={this.handleTextChange}>
+          <input type="text" className="mui-textfield"/>
+          <label>Opponent ID</label>
+        </div>
+        <button className="mui-btn mui-btn--accent" onClick={this.connect}>
+            connect
+        </button>
+      </div>
+    );
+  }
+
   render() {
     var result;
 
     if (this.state.initialized) {
       result = (
         <div>
-          <div>
+          <div className="mui-panel">
             <span>Your connection id is</span>
             <strong className="mui--divider-left">&nbsp;{this.state.my_id}</strong>
           </div>
-          <div>
-
+          <div className="mui-panel">
+            {this.state.connected ? this.renderConnected() : this.renderNotConnected()}
           </div>
         </div>
       );
@@ -67,9 +120,7 @@ class Panel extends React.Component {
     return(
       <div>
         <div className="mui-container">
-          <div className="mui-panel">
-            {result}
-          </div>
+          {result}
         </div>
       </div>
     );
